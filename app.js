ac1db5c5cbeacfd895e6e6a8ca8de7c5d1bdad14
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-06-30 22:14:54 -0300

    commit jsAZ
    commit jsAZ

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+Sistema de cadastro de clientes
+*/
+
+let nome = "Manuel";
+let sobrenome = "Silva";
+let idade = 24;
+const numCliente = 095864;
+let valorEmprestimo = 150000;
+let taxaDeJuros = 10;
+let ehBomPagador = "SIM";
+
+console.log("Boa tarde, Sr. "+ nome +" "+ sobrenome + ". Sua idade é: " +idade)
+
+let nm_produto = "Arroz 1kg"
+let vl_preco = 4.50
+let tp_validade = true
+
+console.log('Produto: '+nm_produto)
+console.log('Preço: '+vl_preco)
+console.log('Validade: '+tp_validade)
+
