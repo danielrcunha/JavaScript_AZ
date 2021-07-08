5e16b41a0e7674330aa48eef7e54007fb18c8543
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-07-07 22:58:23 -0300

    commit
    commit

---
+++ secao_05/scripts/meteo.js
@@ -0,0 +1,24 @@
+//EXEMPLO 01
+var contador = 0
+
+while (contador < 10) {
+    console.log(contador)
+    contador++
+}
+
+//TEMPERATURA ATÉ 
+/*let temp
+let somaTemp = 0
+let numTemp = 0
+
+while (true) {
+    temp = parseInt(prompt("Informe uma temperatura: "))
+    if (temp === 0) {
+        break;
+    }
+    somaTemp += temp;
+    numTemp += 1
+}
+
+console.log(somaTemp / numTemp)*/
+
