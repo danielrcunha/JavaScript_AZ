20d3a619425610b21a1edd8cdb4da7897b629a25
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2021-07-15 20:44:15 -0300

    commit
    commit

--- secao_07/scripts/appFind.js
+++ secao_07/scripts/appFind.js
@@ -1,8 +1,20 @@
 //aula 07 - maus metodos arrays
 
+
+/*
 const numeros = [10, 15, 25, 55, 40];
 const numeroRetornado = numeros.find((numero) => numero > 25);
 console.log(numeroRetornado); //55
+*/
 
+/*
 let indiceRetornado = numeros.findIndex((numero) => numero > 25);
+console.log(indiceRetornado);
+*/
+
+const numeros = [-7, -10, -3, 0, 4, 5, 100];
+
+let numeroRetornado = numeros.find((numero) => numero > 0);
+let indiceRetornado = numeros.findIndex((numero) => numero > 0);
+console.log(numeroRetornado);
 console.log(indiceRetornado);
