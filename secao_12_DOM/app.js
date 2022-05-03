2fe1548b8d1990e3d182c0c4582fe78fc1ce6728
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-05-03 15:00:59 -0300

    commit
    commit

--- secao_12_DOM/app.js
+++ secao_12_DOM/app.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container");
-const listaAtividades = document.querySelector(".lista_atividades");
-const input = document.querySelector(".input");
 const erro = document.querySelector(".erro");
+const input = document.querySelector(".input_div");
 const botaoCadastra = document.querySelector(".botao_adc");
+const listaAtividades = document.querySelector(".lista_atividades");
 const paleta1 = document.querySelector("#paleta1");
 const paleta2 = document.querySelector("#paleta2");
 const paleta3 = document.querySelector("#paleta3");
@@ -46,6 +46,7 @@ function limpaInput() {
     input.value = "";
 }
 
+/*vai validar o "cadastraAtividade()" ao ser clicado*/
 window.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         cadastraAtividade();
