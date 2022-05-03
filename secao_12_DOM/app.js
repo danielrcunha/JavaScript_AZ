2414656446528e19c4d3dad27b95b918eeac8355
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-05-03 15:37:45 -0300

    commit
    commit

--- secao_12_DOM/app.js
+++ secao_12_DOM/app.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container");
-const erro = document.querySelector(".erro");
-const input = document.querySelector(".input_div");
-const botaoCadastra = document.querySelector(".botao_adc");
 const listaAtividades = document.querySelector(".lista_atividades");
+const input = document.querySelector(".input");
+const erro = document.querySelector(".erro");
+const botaoCadastra = document.querySelector(".botao_adc");
 const paleta1 = document.querySelector("#paleta1");
 const paleta2 = document.querySelector("#paleta2");
 const paleta3 = document.querySelector("#paleta3");
@@ -34,7 +34,7 @@ function criaAtividade() {
 function cadastraAtividade() {
     if (input.value.length > 3) {
         erro.style.display = "none";
-        //criaAtividade();
+        criaAtividade();
     } else {
         erro.style.display = "grid";
         erro.innerHTML = `${input.value} não é uma atividade válida!`
